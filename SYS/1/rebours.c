d11ce0f15ec074bd90a955ea9a50d2e0c0cfb9aa
jimmy <100853043+jimmywengzj@users.noreply.github.com>
2023-02-22 16:43:08 +0100

    ex1.1

--- SYS/1/rebours.c
+++ SYS/1/rebours.c
@@ -5,7 +5,18 @@
 
 int main(int argc, char** argv)
 {
-    printf("Bonjour je suis '%s'\n",argv[0]);
+    assert(argc == 2);
+    int duree = atoi(argv[1]);
+    assert(duree > 0);
+
+    //printf("Bonjour je suis '%s'\n",argv[0]);
+    printf("%d: debut\n", getpid());
+    while(duree > 0) {
+        printf("%d: %d\n", getpid(), duree);
+        sleep(1);
+        duree--;
+    }
+    printf("%d: fin\n", getpid());
 
     return EXIT_SUCCESS;
 }
